ef3becbe5abbdb0116e35820089dd038238314e6
nguyentranthevu <nguyentranthevu@gmail.com>
2023-11-02 00:47:01 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import productRouter from "./routes/products.routes.js";
 import orderRouter from "./routes/orders.routes.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
 databaseConnection();
 app.use(express.json());
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
